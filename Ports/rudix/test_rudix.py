b01bb5f321da1cc2d6fc26ca66db2397fca44144
ruda.moura <devnull@localhost>
2010-12-12 14:07:53 +0000

    [NEW] Re-factored using unittest.

--- Ports/rudix/test_rudix.py
+++ Ports/rudix/test_rudix.py
@@ -1,10 +1,15 @@
+import unittest
 from rudix import version_compare
 
-def test_version_compare():
-    assert version_compare('1.0', '2.0') == -1
-    assert version_compare('1.0', '1.0.1') == -1
-    assert version_compare('1.0', '1.0-1') == -1
-    assert version_compare('1.0.2', '1.0.10') == -1
-    assert version_compare('1.0.1-2', '1.0.1-10') == -1
-    # -0 is the same as an empty release number
-    assert version_compare('1.0.1', '1.0.1-0') == 0
+class RudixTest(unittest.TestCase):
+    def test_version_compare(self):
+        self.assertEqual( version_compare('1.0', '2.0'), -1)
+        self.assertEqual( version_compare('1.0', '1.0.1'), -1)
+        self.assertEqual( version_compare('1.0', '1.0-1'), -1)
+        self.assertEqual( version_compare('1.0.2', '1.0.10'), -1)
+        self.assertEqual( version_compare('1.0.1-2', '1.0.1-10'), -1)
+        # -0 is the same as an empty release number)
+        self.assertEqual( version_compare('1.0.1', '1.0.1-0'), 0)
+
+if __name__ == '__main__':
+    unittest.main()
