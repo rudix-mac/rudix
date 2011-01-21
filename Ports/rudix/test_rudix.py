b974ed0315b9c265a5144bafd7973808756f2bc4
Rudá Moura <ruda.moura@gmail.com>
2011-01-20 23:49:47 -0200

    Added tests to communicate and normalization.

--- Ports/rudix/test_rudix.py
+++ Ports/rudix/test_rudix.py
@@ -1,5 +1,5 @@
 import unittest
-from rudix import version_compare
+from rudix import *
 
 class RudixTest(unittest.TestCase):
     def test_version_compare(self):
@@ -10,6 +10,13 @@ class RudixTest(unittest.TestCase):
         self.assertEqual( version_compare('1.0.1-2', '1.0.1-10'), -1)
         # -0 is the same as an empty release number)
         self.assertEqual( version_compare('1.0.1', '1.0.1-0'), 0)
+
+    def test_communicate(self):
+        self.assertEqual(communicate(['echo', 'rudix']), ['rudix'])
+
+    def test_normalization(self):
+        self.assertEqual( normalize('rudix'), 'org.rudix.pkg.rudix' )
+        self.assertEqual( denormalize('org.rudix.pkg.rudix'), 'rudix' )
 
 if __name__ == '__main__':
     unittest.main()
