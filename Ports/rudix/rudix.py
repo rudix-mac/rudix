0e6ea611d390a5f738051b0b69da4be439766e27
'Rudá Moura <ruda.moura@gmail.com>
2011-01-26 22:55:27 -0200

    Fixed the way we compare versions.

--- Ports/rudix/rudix.py
+++ Ports/rudix/rudix.py
@@ -193,9 +193,9 @@ def fix_package(pkg):
 
 def version_compare(v1, v2):
     from distutils.version import LooseVersion
-    # remove the release if it is 0
-    if v1.endswith('-0'): v1 = v1[:-2]
-    if v2.endswith('-0'): v2 = v2[:-2]
+    # Threat revision as part of version
+    v1 = v1.replace('-', '.')
+    v2 = v2.replace('-', '.')
     return cmp(LooseVersion(v1), LooseVersion(v2))
 
 def get_versions_for_package(pkg):
@@ -204,7 +204,7 @@ def get_versions_for_package(pkg):
     content = urlopen('http://code.google.com/p/rudix/downloads/list?q=Filename:%s' % pkg).read()
     urls = re.findall('(http://rudix.googlecode.com/files/(%s-([\w.]+(?:-[0-9]+)?(?:.i386)?)\.dmg))' % pkg, content)
     versions = sorted(list(set(urls)),
-                      cmp=lambda x, y: version_compare(x[1], y[1]))
+                      cmp=lambda x, y: version_compare(x[2], y[2]))
     if len(versions) == 0:
         return []
     else:
--- Ports/rudix/test_rudix.py
+++ Ports/rudix/test_rudix.py
@@ -5,11 +5,11 @@ class RudixTest(unittest.TestCase):
     def test_version_compare(self):
         self.assertEqual( version_compare('1.0', '2.0'), -1)
         self.assertEqual( version_compare('1.0', '1.0.1'), -1)
-        self.assertEqual( version_compare('1.0', '1.0-1'), -1)
         self.assertEqual( version_compare('1.0.2', '1.0.10'), -1)
         self.assertEqual( version_compare('1.0.1-2', '1.0.1-10'), -1)
-        # -0 is the same as an empty release number)
-        self.assertEqual( version_compare('1.0.1', '1.0.1-0'), 0)
+        self.assertEqual(version_compare('1.11-1', '1.11.1-0'), -1)
+        self.assertEqual(version_compare('2.1.0b1-0', '2.1.2-0'), -1)
+        self.assertEqual(version_compare('2.2.1-10', '3.0-0'), -1)
 
     def test_communicate(self):
         self.assertEqual(communicate(['echo', 'rudix']), ['rudix'])
