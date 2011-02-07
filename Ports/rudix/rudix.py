e1c7db2f9ccf1bd09981ce677a3cbbb32cd2eded
Leonardo Santagada <santagada@gmail.com>
2011-02-07 13:45:35 -0200

    quick fix for version select

--- Ports/rudix/rudix.py
+++ Ports/rudix/rudix.py
@@ -215,7 +215,7 @@ def get_versions_for_package(pkg):
     content = urlopen('http://code.google.com/p/rudix/downloads/list?q=Filename:%s' % pkg).read()
     urls = re.findall('(http://rudix.googlecode.com/files/(%s-([\w.]+(?:-\d+)?(?:.i386)?)\.dmg))' % pkg, content)
     versions = sorted(list(set(urls)),
-                      cmp=lambda x, y: version_compare(x[1], y[1]))
+                      cmp=lambda x, y: version_compare(x[2], y[2]))
     if len(versions) == 0:
         return []
     else:
