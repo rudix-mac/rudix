1e4d19bb8fe150e018300df0cd84af0e46e6322e
Leonardo Santagada <santagada@gmail.com>
2011-03-26 18:26:05 -0300

    inproved automation of GNUConfigure, one gcinstallextra hoook a rename of hooks and the asciidoc port

--- Library/GNUConfigure.mk
+++ Library/GNUConfigure.mk
@@ -1,44 +1,48 @@
 
 DISABLE_DEPENDENCY_TRACKING= "yes"
 
-define atconfigure
+define gcconfigure
 env CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)" \
 $(configure) $(CONFIG_OPTS) \
 $(if $DISABLE_DEPENDENCY_TRACKING, --disable-dependency-tracking) 
 endef
 
-define atmake
+define gcmake
 $(make)
 endef
 
 MAKEINSTALLOPTIONS= "DESTDIR=$(INSTALLDIR)"
 
-define atmakeinstall
+define gcmakeinstall
 make $(MAKEINSTALLOPTIONS) install
 endef
 
-define atmakecheck
-make check|| echo "\033[33mOne or more tests failed\033[0m"
+gcinstallextra= ""
+
+
+define gcmakecheck
+make check
 endef
 
 build: prep $(DEPENDS)
-	cd $(BUILDDIR); $(atconfigure)
-	cd $(BUILDDIR); $(atmake)
+	cd $(BUILDDIR); $(gcconfigure)
+	cd $(BUILDDIR); $(gcmake)
 	touch build
 
 install: build
-	cd $(BUILDDIR) ; $(atmakeinstall)
+	cd $(BUILDDIR) ; $(gcmakeinstall)
 	rm -f $(INSTALLDIR)${PREFIX}/share/info/dir
 	rm -f $(INSTALLDIR)${PREFIX}/lib/charset.alias
 	rm -f $(INSTALLDIR)${PREFIX}/share/locale/locale.alias
 	install -d $(INSTALLDOCDIR)
-	for x in $(BUILDDIR)/{COPYING,INSTALL,NEWS,README,LICENSE}; do \
+	for x in $(BUILDDIR)/{CHANGELOG*, BUGS*, COPYING,INSTALL,NEWS,README,LICENSE,NOTICE}; do \
 		if [[ -e $$x ]]; then \
 			install -m 644 $$x $(INSTALLDOCDIR); \
 		fi \
 	done
 	install -m 644 $(README) $(INSTALLDOCDIR)
 	install -m 644 $(LICENSE) $(INSTALLDOCDIR)
+	$(gcinstallextra)
 	if [[ -d $(INSTALLDIR)${PREFIX}/bin ]]; then strip $(INSTALLDIR)$(PREFIX)/bin/*; fi
 	if [[ -d $(INSTALLDIR)${PREFIX}/sbin ]]; then strip $(INSTALLDIR)${PREFIX}/sbin/*; fi
 	if [[ -d $(INSTALLDIR)${PREFIX}/lib ]]; then strip -x $(INSTALLDIR)${PREFIX}/lib/lib*.dylib; fi
@@ -46,5 +50,5 @@ install: build
 	touch install
 
 test: install
-	cd $(BUILDDIR) ; $(atmakecheck)
+	cd $(BUILDDIR) ; $(gcmakecheck) || echo "\033[33mOne or more tests failed\033[0m"
 	touch test
