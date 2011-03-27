fa10a2d061a820e6a6d28b65c7c2fdf54676b9b3
Leonardo Santagada <santagada@gmail.com>
2011-03-26 22:21:44 -0300

    first stab at documenting the new stuff and fixes for GNUConfigure (strip still doesn't work)

--- Library/GNUConfigure.mk
+++ Library/GNUConfigure.mk
@@ -1,5 +1,16 @@
 
-DISABLE_DEPENDENCY_TRACKING= "yes"
+#ifdef WITHOUT_NLS
+#CONFIG_OPTS=	--disable-nls
+#else
+#DEPENDS+=	/usr/local/lib/libintl.la
+#CONFIG_OPTS=	--enable-nls
+#endif
+
+#ifdef WITH_STATIC_NLS
+#LDFLAGS+=	-framework CoreFoundation -liconv
+#endif
+
+DISABLE_DEPENDENCY_TRACKING= yes
 
 define gcconfigure
 env CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)" \
@@ -17,7 +28,7 @@ define gcmakeinstall
 make $(MAKEINSTALLOPTIONS) install
 endef
 
-gcinstallextra= ""
+gcinstallextra=
 
 
 define gcmakecheck
@@ -35,18 +46,24 @@ install: build
 	rm -f $(INSTALLDIR)${PREFIX}/lib/charset.alias
 	rm -f $(INSTALLDIR)${PREFIX}/share/locale/locale.alias
 	install -d $(INSTALLDOCDIR)
-	for x in $(BUILDDIR)/{CHANGELOG*, BUGS*, COPYING,INSTALL,NEWS,README,LICENSE,NOTICE}; do \
+	for x in $(wildcard $(BUILDDIR)/CHANGELOG* \
+						$(BUILDDIR)/BUGS* \
+						$(BUILDDIR)/COPYING \
+						$(BUILDDIR)/INSTALL \
+						$(BUILDDIR)/NEWS \
+						$(BUILDDIR)/README \
+						$(BUILDDIR)/LICENSE \
+						$(BUILDDIR)/NOTICE \
+						$(README) \
+						$(LICENSE)); do \
 		if [[ -e $$x ]]; then \
 			install -m 644 $$x $(INSTALLDOCDIR); \
 		fi \
 	done
-	install -m 644 $(README) $(INSTALLDOCDIR)
-	install -m 644 $(LICENSE) $(INSTALLDOCDIR)
 	$(gcinstallextra)
-	if [[ -d $(INSTALLDIR)${PREFIX}/bin ]]; then strip $(INSTALLDIR)$(PREFIX)/bin/*; fi
-	if [[ -d $(INSTALLDIR)${PREFIX}/sbin ]]; then strip $(INSTALLDIR)${PREFIX}/sbin/*; fi
-	if [[ -d $(INSTALLDIR)${PREFIX}/lib ]]; then strip -x $(INSTALLDIR)${PREFIX}/lib/lib*.dylib; fi
-	if [[ -d $(INSTALLDIR)${PREFIX}/lib ]]; then strip -x $(INSTALLDIR)${PREFIX}/lib/lib*.a; fi
+	for x in $(wildcard $(INSTALLDIR)$(PREFIX)/bin/*); do \
+		 strip $$x; \
+	done 
 	touch install
 
 test: install
