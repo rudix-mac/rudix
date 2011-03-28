2e93b9879a82361516c731160d12b9882efa1c5f
'Rudá Moura <ruda.moura@gmail.com>
2011-03-28 13:19:07 -0300

    Hooks.

--- Library/Python.mk
+++ Library/Python.mk
@@ -20,18 +20,23 @@ $(PYTHON) -m compileall -d / $(INSTALLDIR)
 endef
 
 build: prep $(DEPENDS)
+	$(call pre_build_hook)
 	cd $(BUILDDIR) ; $(pysetupbuild)
+	$(call post_build_hook)
 	touch build
 
 install: build
+	$(call pre_install_hook)
 	cd $(BUILDDIR) ; \
 	$(pysetupinstall) ; $(pycompileall) ; \
 	$(createdocdir)
 	shopt -s nullglob; for x in $(INSTALLDIR)/$(SITEPACKAGES)/*/*.so; do \
 		 strip -x $$x; \
 	done
+	$(call post_install_hook)
 	touch install
 
 test: install
+	$(call pre_test_hook)
+	$(call post_test_hook)
 	touch test
-
