2396db2ce6f1f24faf7550115f1729eea8fa2764
'Rudá Moura <ruda.moura@gmail.com>
2011-06-19 00:21:41 -0300

    rudix.py: Fixed issue 67.

--- Ports/rudix/rudix.py
+++ Ports/rudix/rudix.py
@@ -245,7 +245,7 @@ def get_versions_for_package(pkg):
     'Get a list of available versions for package'
     pkg = denormalize(pkg)
     content = urlopen('http://code.google.com/p/rudix/downloads/list?q=Filename:%s' % pkg).read()
-    urls = re.findall('(http://rudix.googlecode.com/files/(%s-([\w.]+(?:-\d+)?(?:.i386)?)(\.dmg|\.pkg)))' % pkg, content)
+    urls = re.findall('(rudix.googlecode.com/files/(%s-([\w.]+(?:-\d+)?(?:.i386)?)(\.dmg|\.pkg)))' % pkg, content)
     versions = sorted(list(set(urls)),
                       cmp=lambda x, y: version_compare(x[2], y[2]))
     if len(versions) == 0:
