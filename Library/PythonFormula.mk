2d1344b22720528cb7e5e4491bb17b410a10815f
'Rudá Moura <ruda.moura@gmail.com>
2011-08-31 23:36:30 -0300

    BuildSystem :updated PythonFormula to use only Python 2.7 (on Lion).

--- Library/PythonFormula.mk
+++ Library/PythonFormula.mk
@@ -5,37 +5,34 @@
 #
 # Python versions
 #
+# Default
 Python = $(Python2.7)
 PythonSitePackages = $(PythonSitePackages2.7)
+# 2.7
 Python2.7 = /usr/bin/python2.7
 PythonSitePackages2.7 = /Library/Python/2.7/site-packages
+# 2.6
 Python2.6 = /usr/bin/python2.6
 PythonSitePackages2.6 = /Library/Python/2.6/site-packages
 
 define build_inner_hook
 cd $(BuildDir) ; \
-env CFLAGS="$(CFlags)" CXXFLAGS="$(CxxFlags)" LDFLAGS="$(LdFlags)" ARCHFLAGS="$(ArchFlags)" $(EnvExtra) \
-$(Python2.6) setup.py build $(SetupExtra)
-cd $(BuildDir) ; \
-env CFLAGS="$(CFlags)" CXXFLAGS="$(CxxFlags)" LDFLAGS="$(LdFlags)" ARCHFLAGS="$(ArchFlags)" $(EnvExtra) \
-$(Python2.7) setup.py build $(SetupExtra)
+env CFLAGS="$(CFlags)" \
+	CXXFLAGS="$(CxxFlags)" \
+	LDFLAGS="$(LdFlags)" \
+	ARCHFLAGS="$(ArchFlags)" $(EnvExtra) \
+$(Python) setup.py build $(SetupExtra)
 endef
 
 define install_inner_hook
 cd $(BuildDir) ; \
-$(Python2.6) setup.py install $(SetupInstallExtra) \
+$(Python) setup.py install $(SetupInstallExtra) \
 	--no-compile \
 	--root=$(PortDir)/$(InstallDir) \
 	--prefix=$(Prefix) \
-	--install-lib=$(PythonSitePackages2.6)
-cd $(BuildDir) ; $(Python2.6) -m compileall -d / $(PortDir)/$(InstallDir)
+	--install-lib=$(PythonSitePackages)
 cd $(BuildDir) ; \
-$(Python2.7) setup.py install $(SetupInstallExtra) \
-	--no-compile \
-	--root=$(PortDir)/$(InstallDir) \
-	--prefix=$(Prefix) \
-	--install-lib=$(PythonSitePackages2.7)
-cd $(BuildDir) ; $(Python2.7) -m compileall -d / $(PortDir)/$(InstallDir)
+$(Python) -m compileall -d / $(PortDir)/$(InstallDir)
 $(install_base_documentation)
 endef
 
