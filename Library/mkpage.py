901239c0965772802bbb36663e698f922a44a5c6
Rudá Moura <ruda.moura@gmail.com>
2013-03-24 11:47:31 -0300

    Update mkpage (include downloads).

--- Library/mkpage.py
+++ Library/mkpage.py
@@ -12,6 +12,12 @@ description: {Summary}
 # {Name}: {Title} #
 
 {Description}
+
+## Downloads ##
+
+* [{PkgFile}](http://code.google.com/p/rudix-mountainlion/downloads/detail?name={PkgFile}) | OS X Mountain Lion (10.8) | [Previous versions](http://code.google.com/p/rudix-mountainlion/downloads/list?can=2&q={Name})
+* [{PkgFile}](http://code.google.com/p/rudix/downloads/detail?name={PkgFile}) | Mac OS X Lion (10.7) | [Previous versions](http://code.google.com/p/rudix/downloads/list?can=2&q={Name})
+* [{PkgFile}](http://code.google.com/p/rudix-snowleopard/downloads/detail?name={PkgFile}) | Mac OS X Snow Leopard (10.6) | [Previous versions](http://code.google.com/p/rudix-snowleopard/downloads/list?can=2&q={Name})
 '''
 
 class Global_Env_Dict(object):
