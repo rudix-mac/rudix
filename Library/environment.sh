fb87508370bada286e8c7fc2d73704d4a9ca410c
Rudá Moura <ruda.moura@gmail.com>
2013-04-07 17:59:17 -0300

    Update environment.sh script.

--- Library/environment.sh
+++ Library/environment.sh
@@ -1,4 +1,6 @@
-LANG=C
-PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+# Safe environment variables to build packages
+LANG="C"
+PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
 PS1="[Rudix] $PS1"
 export LANG PATH
+unset EDITOR ALTERNATE_EDITOR PYTHONSTARTUP
