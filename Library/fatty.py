a3ab1ad4168dd19b70d16a73cb1f8216704cb45d
Rudá Moura <ruda.moura@gmail.com>
2016-12-14 19:14:10 -0200

    fatty.py: Refactored code.
    Signed-off-by: Rudá Moura <ruda.moura@gmail.com>

--- Library/fatty.py
+++ Library/fatty.py
@@ -5,44 +5,33 @@
 #
 #            “You're the One for me, fatty” (Morrissey)
 #
-# Copyright © 2013-2014 Rudix
+# Copyright © 2013-2016 Rudá Moura
 # Author: Rudá Moura <ruda.moura@gmail.com>
+#
 
-"""Test if a given file is Universal Binary (Fat Binary)."""
+"""Test if a given file is Fat Binary.
+
+Test if a given file is Fat Binary (Universal Binary).
+
+Return code:
+0 = Non-fat file.
+1 = Fat file."""
 
 import sys
 import os
 from platform import mac_ver
 
-ProductVersion = mac_ver()[0]
 
-def _lipo_command(osx_version=ProductVersion):
+def fatty(path, osx_version):
     if osx_version.startswith('10.5.'):
-        cmd = 'lipo {path} -verify_arch i386 ppc7400 || lipo {path} -verify_arch i386 ppc'
-    elif osx_version.startswith('10.6.'):
-        cmd = 'lipo {path} -verify_arch x86_64 i386'
-    elif osx_version.startswith('10.7.'):
-        cmd = 'lipo {path} -verify_arch x86_64 i386'
-    elif osx_version.startswith('10.8.'):
-        cmd = 'lipo {path} -verify_arch x86_64 i386'
-    elif osx_version.startswith('10.9.'):
-        cmd = 'lipo {path} -verify_arch x86_64 i386'
-    elif osx_version.startswith('10.10.'):
-        cmd = 'lipo {path} -verify_arch x86_64 i386'
-    elif osx_version.startswith('10.11.'):
-        cmd = 'lipo {path} -verify_arch x86_64 i386'
+        lipo = 'lipo {path} -verify_arch i386 ppc7400 || lipo {path} -verify_arch i386 ppc'
     else:
-        cmd = 'lipo {path} -verify_arch i386 ppc'
-    return cmd
-
-def fatty(path):
-    lipo = _lipo_command()
+        lipo = 'lipo {path} -verify_arch x86_64 i386'
     lipo = lipo.format(path=path)
     result = os.system(lipo)
     return result >> 8
 
+
 if __name__ == '__main__':
     if sys.argv[1:]:
-        sys.exit(fatty(sys.argv[1]))
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+        sys.exit(fatty(sys.argv[1], mac_ver()[0]))
