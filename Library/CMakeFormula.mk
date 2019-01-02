d19e531d09dc0c4e9bf3ac616875854c7bb1c6da
Rudá Moura <ruda.moura@gmail.com>
2019-01-01 22:05:02 -0200

    Support for CMake builds (CMake.mk)

---
+++ Library/CMakeFormula.mk
@@ -0,0 +1,31 @@
+#
+# CMake Formula.
+#
+# Copyright © 2019 Rudix
+# Author: Rudá Moura <ruda.moura@gmail.com>
+#
+
+CMakeExtra += -DCMAKE_BUILD_TYPE=Release
+CMakeExtra += -DCMAKE_INSTALL_PREFIX=$(Prefix)
+
+define build_hook
+cd $(BuildDir) && \
+env $(EnvExtra) cmake $(CMakeExtra)
+endef
+
+define install_hook
+cd $(BuildDir) && \
+$(make_install) DESTDIR="$(DestDir)" $(MakeInstallExtra)
+$(install_base_documentation)
+$(strip_macho)
+endef
+
+ifeq ($(RUDIX_RUN_ALL_TESTS),yes)
+define check_hook
+cd $(BuildDir) && $(MAKE) test || $(call error_color,One or more tests failed)
+endef
+endif
+
+buildclean:
+	cd $(BuildDir) && $(MAKE) clean || $(call warning_color,Cannot clean)
+	rm -f build check
