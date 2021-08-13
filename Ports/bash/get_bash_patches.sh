cee98e60d8851cadb6ad74b5b6f3214a7e95dd7d
Rudá Moura <ruda.moura@gmail.com>
2021-08-13 12:58:08 -0300

    bash: update to version 4.4.18

--- Ports/bash/get_bash_patches.sh
+++ Ports/bash/get_bash_patches.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-BASH_PATCHES="http://ftp.gnu.org/gnu/bash/bash-4.3-patches/"
+BASH_PATCHES="http://ftp.gnu.org/gnu/bash/bash-4.4-patches/"
 
 for i in `jot 30`
 do
     N=`printf "%0.3d" $i`
-    curl -O $BASH_PATCHES/bash43-$N
-    curl -O $BASH_PATCHES/bash43-$N.sig
+    curl -O $BASH_PATCHES/bash44-$N
+    curl -O $BASH_PATCHES/bash44-$N.sig
 done
